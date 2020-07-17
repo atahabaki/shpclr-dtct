3e57ec696025dce7d96c449d4aac2a157e975fbb
A. Taha Baki <a.taha.baki@outlook.com>
2020-07-17 23:49:13 +0300

    +2 mthds

--- src/detect.py
+++ src/detect.py
@@ -31,7 +31,11 @@ class Detective:
         self.data = data
         self.verbose = verbose
 
-    def detectViaThreshold(self,picSize=(720,405),drawContours=True):
+    def resizeImg(self, dimens):
+        return cv.resize(self.data,dimens)
+    def blurImg(self, img):
+        return cv.GaussianBlur(img,(7,7),1)
+    def detectByThresholding(self,dimens=(720,405),drawContours=True):
         """
         Detects object returns resized and blurred image, and X,Y coordinates of detected object
         Returns:
@@ -39,11 +43,11 @@ class Detective:
         (image, x, y)
         Arguments:
         ==========
-        picSize (width, height): Determines the resize options just width & height
+        dimens (width, height): Determines the resize options just width & height
         drawContours (bool): Draws contours if set to True
         """
-        img = cv.resize(self.data,picSize)
-        img = cv.GaussianBlur(img,(7,7),1)
+        img = self.resizeImg(dimens)
+        img = self.blurImg(img)
         gry = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         _, thresh = cv.threshold(gry, self.shape.tmin, self.shape.tmax, cv.THRESH_BINARY_INV)
         contours, _ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
@@ -53,10 +57,71 @@ class Detective:
             area = cv.contourArea(approx)
             if self.verbose:
                 print("area",area)
-            if area <= self.shape.amax and area >= self.shape.amin:
+            if area >= self.shape.amin and area <= self.shape.amax:
                 M = cv.moments(approx)
                 cx, cy = (int(M['m10']/M['m00']),int(M['m01']/M['m00']))
                 print("center:",(cx,cy))
                 if drawContours:
                     cv.drawContours(img,[approx],-1,(0,0,0),10)
+        return (img, cx, cy)
+    def detectByColor(self,dimens=(720,405),erode=True,drawContours=True):
+        """
+        Detects the object by it's color and approximate area
+        Returns:
+        ========
+        (image, x, y)
+        Arguments:
+        ==========
+        dimens (width, height): Determines the resize options just width & height
+        erode (bool): Removes noises smaller or equal to 20x20 if set to True
+        drawContours (bool): Draws contours if set to True
+        """
+        img = self.resizeImg(dimens)
+        img = self.blurImg(img)
+        mask = cv.inRange(img, self.shape.lowerb, self.shape.upperb)
+        if erode:
+            mask = cv.erode(mask, np.ones((20,20),np.uint8))
+        contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        cx,cy=None,None
+        for cnt in contours:
+            approx = cv.approxPolyDP(cnt, .001*cv.arcLength(cnt, True), True)
+            area = cv.contourArea(approx)
+            if self.verbose:
+                print("area",area)
+            if area >= self.shape.amin and area <= self.shape.amax:
+                M = cv.moments(approx)
+                cx, cy = (int(M['m10']/M['m00']),int(M['m01']/M['m00']))
+                print("center:",(cx,cy))
+                if drawContours:
+                    cv.drawContours(img,[approx],-1,(0,0,0),10)
+        return (img, cx, cy)
+    def detectByColorOnly(self,dimens=(720,405),erode=True,drawContours=True):
+        """
+        Detects the object by it's color only
+        Returns:
+        ========
+        (image, x, y)
+        Arguments:
+        ==========
+        dimens (width, height): Determines the resize options just width & height
+        erode (bool): Removes noises smaller or equal to 20x20 if set to True
+        drawContours (bool): Draws contours if set to True
+        """
+        img = self.resizeImg(dimens)
+        img = self.blurImg(img)
+        mask = cv.inRange(img, self.shape.lowerb, self.shape.upperb)
+        if erode:
+            mask = cv.erode(mask, np.ones((20,20),np.uint8))
+        contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        cx,cy=None,None
+        for cnt in contours:
+            approx = cv.approxPolyDP(cnt, .001*cv.arcLength(cnt, True), True)
+            area = cv.contourArea(approx)
+            if self.verbose:
+                print("area",area)
+            M = cv.moments(approx)
+            cx, cy = (int(M['m10']/M['m00']),int(M['m01']/M['m00']))
+            print("center:",(cx,cy))
+            if drawContours:
+                cv.drawContours(img,[approx],-1,(0,0,0),10)
         return (img, cx, cy)
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 
 class Shape:
-    def __init__(self, tmin, tmax, amin, amax):
+    def __init__(self, tmin, tmax, amin, amax, lowerb=None, upperb=None):
         """
         Args:
         =====
@@ -10,11 +10,15 @@ class Shape:
         tmax: Threshold min val
         amin: min val of the object's area
         amax: max val of the object's area
+        lowerb: BGR color code of the object's color
+        upperb: BGR color code of the object's color
         """
         self.tmin = tmin
         self.tmax = tmax
         self.amin = amin
         self.amax = amax
+        self.lowerb = lowerb
+        self.upperb = upperb
 
 class Detective:
     def __init__(self, shape, data, verbose=True):
@@ -31,7 +35,11 @@ class Detective:
         self.data = data
         self.verbose = verbose
 
-    def detectViaThreshold(self,picSize=(720,405),drawContours=True):
+    def resizeImg(self, dimens):
+        return cv.resize(self.data,dimens)
+    def blurImg(self, img):
+        return cv.GaussianBlur(img,(7,7),1)
+    def detectByThresholding(self,dimens=(720,405),drawContours=True):
         """
         Detects object returns resized and blurred image, and X,Y coordinates of detected object
         Returns:
@@ -39,11 +47,11 @@ class Detective:
         (image, x, y)
         Arguments:
         ==========
-        picSize (width, height): Determines the resize options just width & height
+        dimens (width, height): Determines the resize options just width & height
         drawContours (bool): Draws contours if set to True
         """
-        img = cv.resize(self.data,picSize)
-        img = cv.GaussianBlur(img,(7,7),1)
+        img = self.resizeImg(dimens)
+        img = self.blurImg(img)
         gry = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         _, thresh = cv.threshold(gry, self.shape.tmin, self.shape.tmax, cv.THRESH_BINARY_INV)
         contours, _ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
@@ -53,18 +61,89 @@ class Detective:
             area = cv.contourArea(approx)
             if self.verbose:
                 print("area",area)
-            if area <= self.shape.amax and area >= self.shape.amin:
+            if area >= self.shape.amin and area <= self.shape.amax:
                 M = cv.moments(approx)
                 cx, cy = (int(M['m10']/M['m00']),int(M['m01']/M['m00']))
                 print("center:",(cx,cy))
                 if drawContours:
                     cv.drawContours(img,[approx],-1,(0,0,0),10)
         return (img, cx, cy)
+    def detectByColor(self,dimens=(720,405),erode=True,drawContours=True):
+        """
+        Detects the object by it's color and approximate area
+        Returns:
+        ========
+        (image, x, y)
+        Arguments:
+        ==========
+        dimens (width, height): Determines the resize options just width & height
+        erode (bool): Removes noises smaller or equal to 20x20 if set to True
+        drawContours (bool): Draws contours if set to True
+        """
+        img = self.resizeImg(dimens)
+        img = self.blurImg(img)
+        mask = cv.inRange(img, self.shape.lowerb, self.shape.upperb)
+        if erode:
+            mask = cv.erode(mask, np.ones((20,20),np.uint8))
+        contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        cx,cy=None,None
+        for cnt in contours:
+            approx = cv.approxPolyDP(cnt, .001*cv.arcLength(cnt, True), True)
+            area = cv.contourArea(approx)
+            if self.verbose:
+                print("area",area)
+            if area >= self.shape.amin and area <= self.shape.amax:
+                M = cv.moments(approx)
+                cx, cy = (int(M['m10']/M['m00']),int(M['m01']/M['m00']))
+                print("center:",(cx,cy))
+                if drawContours:
+                    cv.drawContours(img,[approx],-1,(0,0,0),10)
+        return (img, cx, cy)
+    def detectByColorOnly(self,dimens=(720,405),erode=True,drawContours=True):
+        """
+        Detects the object by it's color only
+        Returns:
+        ========
+        (image, x, y)
+        Arguments:
+        ==========
+        dimens (width, height): Determines the resize options just width & height
+        erode (bool): Removes noises smaller or equal to 20x20 if set to True
+        drawContours (bool): Draws contours if set to True
+        """
+        img = self.resizeImg(dimens)
+        img = self.blurImg(img)
+        mask = cv.inRange(img, self.shape.lowerb, self.shape.upperb)
+        if erode:
+            mask = cv.erode(mask, np.ones((20,20),np.uint8))
+        contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        cx,cy=None,None
+        for cnt in contours:
+            approx = cv.approxPolyDP(cnt, .001*cv.arcLength(cnt, True), True)
+            area = cv.contourArea(approx)
+            if self.verbose:
+                print("area",area)
+            M = cv.moments(approx)
+            cx, cy = (int(M['m10']/M['m00']),int(M['m01']/M['m00']))
+            print("center:",(cx,cy))
+            if drawContours:
+                cv.drawContours(img,[approx],-1,(0,0,0),10)
+        return (img, cx, cy)
 
-shape = Shape(150,255,100,100000)
-detective = Detective(shape,cv.imread("media/out3.png"))
-img, cx, cy = detective.detectViaThreshold()
+
+shape = Shape(150,255,60000,100000,np.array([100,100,100]),np.array([180,180,180]))
+detective = Detective(shape,cv.imread("media/out2.png"))
+img, cx, cy = detective.detectByColor()
 
 cv.imshow("img", img)
 cv.waitKey(0)
 cv.destroyAllWindows()
+"""
+shape = Shape(150,255,100,100000)
+detective = Detective(shape,cv.imread("media/out3.png"))
+img, cx, cy = detective.detectByThresholding()
+
+cv.imshow("img", img)
+cv.waitKey(0)
+cv.destroyAllWindows()
+"""
